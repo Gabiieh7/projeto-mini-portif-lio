3177f6ff829ada3985082462fa97284576b0844c
Gabriela Paula <gabrielapaula76667@gmail.com>
2022-10-20 18:21:52 -0300

    melhorando o js

--- src/js/index.js
+++ src/js/index.js
@@ -1,22 +1,50 @@
-const CoracaoAvancar = document.querySelector('.coracaoavancar');
-const SobreVoltar = document.querySelector('.sobrevoltar');
-const Contato = document.querySelector('.contato');
-const Sobre = document.querySelector('.sobre');
+//primeira forma: feita por mim sozinha sem estudar
+
+/*const coracaoAvancar = document.querySelector('.coracaoavancar');
+const sobreVoltar = document.querySelector('.sobrevoltar');
+const contato = document.querySelector('.contato');
+const sobre = document.querySelector('.sobre');
 
 CoracaoAvancar.addEventListener('click', function () {
-    Contato.classList.remove('nao-selecionado');
-    Contato.classList.add('selecionado');
-    Sobre.classList.remove('selecionado');
-    Sobre.classList.add('nao-selecionado');
-    SobreVoltar.classList.remove('selecionado');
-    CoracaoAvancar.classList.add('selecionado');
+    contato.classList.remove('nao-selecionado');
+    contato.classList.add('selecionado');
+    sobre.classList.remove('selecionado');
+    sobre.classList.add('nao-selecionado');
+    sobreVoltar.classList.remove('selecionado');
+    coracaoAvancar.classList.add('selecionado');
 
 })
 SobreVoltar.addEventListener('click', function() {
-    Contato.classList.remove('selecionado');
-    Contato.classList.add('nao-selecionado');
-    Sobre.classList.remove('nao-selecionado');
-    Sobre.classList.add('selecionado');
-    CoracaoAvancar.classList.remove('selecionado');
-    SobreVoltar.classList.add('selecionado');
-})
+    contato.classList.remove('selecionado');
+    contato.classList.add('nao-selecionado');
+    sobre.classList.remove('nao-selecionado');
+    sobre.classList.add('selecionado');
+    coracaoAvancar.classList.remove('selecionado');
+    sobreVoltar.classList.add('selecionado');
+})*/
+
+//segunda forma: depois de assistir a mapadevweek
+
+const abas = document.querySelectorAll('.aba');
+
+function selecionarAba(aba){
+    const abaSelecionada =document.querySelector('.aba.selecionado');
+    abaSelecionada.classList.remove('selecionado');
+    aba.classList.add('selecionado');
+}
+
+function mostrarInformacoesDaAba(aba){
+    const idDoElementoDeInformacoesDaAba = `informacao-${aba.id}`;
+    const informacaoASerMostrada = document.getElementById(idDoElementoDeInformacoesDaAba);
+    const informacaoSelecionada = document.querySelector('.informacao.selecionado');
+    informacaoSelecionada.classList.remove('selecionado');
+    informacaoASerMostrada.classList.add('selecionado');
+}
+
+abas.forEach((aba)=>{
+    aba.addEventListener('click', function () {
+        if(aba.classList.contains('selecionado')) return;
+        selecionarAba(aba);
+        mostrarInformacoesDaAba(aba);
+    });
+});
